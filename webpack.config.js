d29da40d46fbb7318a91c79c090d3d82ba6c1dff
Dossbaby <doss@dossbaby.com>
2020-02-06 00:59:33 +0900

    Completed hero block and upper content block

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+];
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch("./app/**/*.html")
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
+        ]
+      }
+    ]
+  }
+};
