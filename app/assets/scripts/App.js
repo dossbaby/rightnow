c95a5b270b0897c862a1251c57b0bb2bc0bf98f0
Dossbaby <doss@dossbaby.com>
2020-02-06 23:02:48 +0900

    sticky menu color change and mobile menu fix

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,8 +4,8 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader;
-let mobileMenu;
+let stickyHeader = new StickyHeader();
+let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
 let modal;
