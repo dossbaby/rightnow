cb53d32b4751d41608842f01a7b3a56b41e7ada0
Dossbaby <doss@dossbaby.com>
2020-02-06 22:15:38 +0900

    Updated cloud function to check super secret password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  });
+  const secretContent = `
+  <h3>The Secret Area</h3>
+  <p>Welcome to the <strong>secret area</strong> in the cloud.</p>
+  `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    });
+  } else {
+    callback(null, {
+      statusCode: 401
+    });
+  }
 };
