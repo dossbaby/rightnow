6bcedba922398dd8550cdf30e12ed172d6a96ef6
Dossbaby <doss@dossbaby.com>
2020-02-06 17:07:50 +0900

    Added fade in for Features and Testimonials

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
 
 if (module.hot) {
   module.hot.accept();
